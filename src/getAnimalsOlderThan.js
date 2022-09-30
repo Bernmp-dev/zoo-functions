4f93cabccc7c12011023cb605b5b592cc1d9539f
Bernardo Marques <bernardomp.dev@gmail.com>
2022-09-29 22:26:55 -0300

    2nd requirement completed

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, age) => data.species.find((element) =>
+  (animal.includes(element.name))).residents.every((media) => media.age > age);
 
 module.exports = getAnimalsOlderThan;
+
+console.log(getAnimalsOlderThan('tigers', 19));
