962ca729aa206b09ce1928b9587522cc9e1e08aa
Bernardo Marques <bernardomp.dev@gmail.com>
2022-09-30 16:37:17 -0300

    3rd requirement completed

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (...employeeName) =>
+  ((employeeName.length === 0) ? {} : data.employees.find((selected) =>
+    employeeName.includes(selected.lastName) || employeeName.includes(selected.firstName)));
+
+console.log(getEmployeeByName('Nigel'));
 
 module.exports = getEmployeeByName;
