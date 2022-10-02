3ec7fd25f43c490c4d6ab6ca798124dfc9d05b5d
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-02 00:00:18 -0300

    5th requirement completed

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return Object.fromEntries(species.map((curr) => [curr.name, curr.residents.length]));
+  }
+  if (Object.keys(animal).length === 1) {
+    return species.find((curr) => animal.specie.includes(curr.name))
+      .residents.length;
+  }
+  return species.find((curr) => curr.name.includes(animal.specie))
+    .residents.filter((curr2) => curr2.sex === animal.sex).length;
 }
+
+console.log(countAnimals({ specie: 'elephants', sex: 'female' }));
 
 module.exports = countAnimals;
