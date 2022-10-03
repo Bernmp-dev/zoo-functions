b166c539bee2cbfc7ed06d2f405fbf7a81c1e7b0
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-03 15:50:23 -0300

    6th, 7th n 15th requirements completed.

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,15 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Testando funcao etapa por etapa', () => {
+    expect(handlerElephants()).toBeUndefined();
+    expect(handlerElephants('string')).not.toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants(2)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toContain('Jefferson');
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+    expect(handlerElephants('location')).toBe('NW');
+    expect(handlerElephants('popularity')).toBeGreaterThanOrEqual(5);
+    expect(handlerElephants('availability')).not.toContain('Monday');
+  });
+});
