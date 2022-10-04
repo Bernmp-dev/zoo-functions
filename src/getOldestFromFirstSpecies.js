cc4cc090ca9580b05815f5f8677f409f80088f65
Bernardo Marques <bernardomp.dev@gmail.com>
2022-10-04 18:12:31 -0300

    10th requirement completed

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = employees.find((curr) => curr.id
+    .includes(id)).responsibleFor[0];
+
+  return Object.values(species.find((curr) => curr.id.includes(animalId))
+    .residents.reduce((acc, curr) => ((curr.age < acc.age) ? acc : curr)));
 }
 
 module.exports = getOldestFromFirstSpecies;
